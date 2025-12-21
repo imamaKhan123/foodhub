2d1a0236625af7c03d31cb0510ba7c116eb4f38f
användare <anvandare@MacBook-Pro-som-tillhor-anvandare.local>
2025-12-21 16:20:18 +0100

    data

--- src/Data/SampleData.ts
+++ src/Data/SampleData.ts
@@ -651,8 +651,8 @@ export const MENU_DATA: MenuItem[] = [
     id: '1104',
     name: 'Peri Peri Quarter Chicken',
     description: 'Grilled peri peri quarter chicken',
-     image: 'https://images.unsplash.com/photo-1712579733874-c3a79f0f9d12?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxncmlsbGVkJTIwY2hpY2tlbnxlbnwxfHx8fDE3NjU4MjAyNTR8MA&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral',
-
+     image:  'https://images.unsplash.com/photo-1712579733874-c3a79f0f9d12?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxncmlsbGVkJTIwY2hpY2tlbnxlbnwxfHx8fDE3NjU4MjAyNTR8MA&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral',
+      
     category: 'Peri Peri',
     basePrice: 3.99,
     addOns: [
